d6c18917389d0c78b33d403480b01be3fcef5497
francescorinaldi <frarino1002@gmail.com>
2020-09-14 17:58:23 +0100

    first heroku deployment

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import './App.css';
 import {BrowserRouter as Router, Route} from "react-router-dom" ;
 import "bootstrap/dist/css/bootstrap.min.css"
@@ -7,7 +6,6 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import NavBar from "./components/NavBar/navbar.component";
 import Home from "./components/Home/home.component";
 import Test from "./components/Test/test.component";
-
 
 function App() {
   return (
--- src/components/NavBar/navbar.component.js
+++ src/components/NavBar/navbar.component.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 import './navbar.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -17,7 +16,7 @@ export default class Navbar extends Component {
         <a className="navbar-brand" href="/">EPS</a>
         <ul id="list"  className="navbar-nav mr-auto mt-2 mt-lg-0">
           <li className="nav-item active">
-            <a className="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+            <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
           </li>
           <li className="nav-item active">
             <a className="nav-link" href="/test">Test</a>
