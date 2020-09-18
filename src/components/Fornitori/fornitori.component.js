60ef4a8225d6fe8731965df8982e18fce8146735
rin0o1 <frarino1002@gmail.com>
2020-09-18 16:50:20 +0100

    post on adding

--- src/components/Fornitori/fornitori.component.js
+++ src/components/Fornitori/fornitori.component.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import  BaseComponent from "../Shared/base.component.js";
+import {Form} from 'react-advanced-form';
+import {Input} from 'react-advanced-form-addons';
 import axios from "axios";
 import FooterModel from "../../models/footer.model";
 import '../Fornitori/fornitori.css';
@@ -10,7 +12,14 @@ export default class Fornitori extends BaseComponent {
     super(props);               
     this.state = { 
         url : process.env.REACT_APP_DATAURL_FORNITORI_PROD,
-        result: [],        
+        addurl:process.env.REACT_APP_DATAURL_ADDFORNITORI_LOCAL,
+        result: [],
+        name :"", 
+        phoneNumber:"",
+        emailAddress: "",
+        street:"",
+        postcode:"",
+        city:""
     };                   
   }         
 
@@ -29,17 +38,131 @@ export default class Fornitori extends BaseComponent {
             result:x.data,                            
         })});
   } 
-   
 
+  
+  async  makeRequest (data) {
+    return await axios.post(this.state.addurl, data)
+
+  }
+
+  submitForm = () =>{                
+       
+    console.log("2here");
+    
+    var fileToPost={
+      name:this.state.name,
+      phoneNumber:this.state.phoneNumber,
+      emailAddress:this.state.emailAddress,
+      street:this.state.street,
+      city:this.state.city,
+      postcode:this.state.postcode
+    };
+
+    
+    alert (this.makeRequest(fileToPost));
+
+    this.closeCreateDialog();   
+
+    window.location.reload(false);
+    
+  }
+
+  setName = (e) => this.setState({ name : e.target.value });
+  setphoneNumber = (e) => this.setState({ phoneNumber: e.target.value });
+  setemailAddress = (e) => this.setState({ emailAddress: e.target.value });
+  setstreet = (e) => this.setState({ street: e.target.value });
+  setcity = (e) => this.setState({ city: e.target.value });
+  setpostcode = (e) => this.setState({ postcode: e.target.value });
+  
+   
   create(){
     //add ! 
     if(!this.state.showCreateDialog) return;
     return(
-      <div  id="createDialog" className="createDialog" >        
+      <div  id="createDialog" className="createDialog">        
+     <form className="marginTop" >
+          <div className="col-md-12">
+            <div className="row">              
+              <label className="col-md-3">
+                NOME             
+              </label>
+              <label className="col-md-3">
+                CELL
+              </label>
+              <label className="col-md-6">
+                EMAIL             
+              </label>          
+            </div>
+
+            <div className="row">
+              <input  className="col-md-3" 
+                      type="text" name="name" 
+                      value={this.state.name} 
+                      onChange={this.setName}
+                      />
+              <input  className="col-md-3" 
+                      type="text" 
+                      name="phoneNumber" 
+                      value={this.state.phoneNumber}
+                      onChange={this.setphoneNumber}
+                      />
+              <input  className="col-md-6" 
+                      type="text" 
+                      name="emailAddress"
+                      value= {this.state.emailAddress}
+                      onChange={this.setemailAddress}
+                       />            
+            </div>                                
+          </div>
+
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+
+          <div className="col-md-12">
+            <div className="row">              
+              <label className="col-md-5">
+                VIA
+              </label>
+              <label className="col-md-4">
+                CITTA
+              </label>
+              <label className="col-md-3">
+                POSTCODE             
+              </label>          
+            </div>
+            <div className="row" name="address">
+              <input  className="col-md-5" 
+                      type="text" 
+                      name="street" 
+                      value={this.state.street}
+                      onChange={this.setstreet}
+                      />
+              <input  className="col-md-4" 
+                      type="text" 
+                      name="city" 
+                      value= {this.state.city}
+                      onChange={this.setcity}
+                      />
+              <input  className="col-md-3" 
+                      type="text" 
+                      name="postcode" 
+                      value={this.state.postcode}
+                      onChange={this.setpostcode}
+                      />            
+            </div>                                
+          </div>
+
+
         <div className="buttonContainer">
-          <button className="myButton saveButton" > SAVE </button>
+          <button className="myButton saveButton"  onClick={()=> this.submitForm()}  > SAVE </button>
           <button className="myButton closeButton" onClick={() =>this.closeCreateDialog()}> CLOSE </button>          
         </div>
+          
+        </form>                
+
+
         
       </div>
     )
@@ -65,7 +188,7 @@ export default class Fornitori extends BaseComponent {
           <tbody>        
           {this.state.result && this.state.result.map(x=>{                                        
                   return (
-                    <tr id={x._id}>
+                    <tr key={x._id}>
                     <td> {x.name} </td>
                     <td> {x.phoneNumber} </td>
                     <td> {x.emailAddress} </td>
