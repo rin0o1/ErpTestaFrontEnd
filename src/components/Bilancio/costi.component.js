82a5aaed3d4f427de15a9ff2add916ed8c8eab4c
rin0o1 <frarino1002@gmail.com>
2020-10-25 16:51:59 +0000

    cost bilancio

--- src/components/Bilancio/costi.component.js
+++ src/components/Bilancio/costi.component.js
@@ -7,7 +7,7 @@ export default class Costi extends BaseComponent {
   constructor(props) {
     super(props);
     this.state = {
-      url: "https:/erptestabackend.herokuapp.com/bilancio",
+      url: "https://erptestabackend.herokuapp.com/bilancio",
       data: [],
     };
   }
@@ -19,6 +19,7 @@ export default class Costi extends BaseComponent {
       });
     });
   }
+  z;
 
   render() {
     return (
